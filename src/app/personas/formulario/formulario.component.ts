12e44899856ef8cd0a79064d586f26c8b658b4ad
Erika <erikac.ec80@gmail.com>
2022-05-06 17:34:33 -0300

    editar elementos

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoggingService } from '../../LoggingService.service';
 import { Persona } from '../../persona.model';
 import { PersonasService } from '../../personas.service';
@@ -13,25 +13,38 @@ export class FormularioComponent implements OnInit {
 
   nombreInput:string = '';
   apellidoInput:string = '';
+  index: number;
   
   constructor(
     private loggingService:LoggingService,
     private personasService: PersonasService,
-    private router: Router) {
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
       this.personasService.saludar.subscribe(
         (indice:number) => alert("El indice es: " + indice)
       );
     }
   
   ngOnInit() {
-
+    this.index = this.route.snapshot.params['id'];
+    if(this.index){
+      let persona: Persona = this.personasService.encontrarPersona(this.index);
+      this.nombreInput = persona.nombre;
+      this.apellidoInput = persona.apellido;
+    }
   }
   
   onGuardarPersona(){
     let persona1 = new Persona(
       this.nombreInput, this.apellidoInput
     );
-    this.personasService.agregarPersona(persona1);
+    if(this.index){
+      this.personasService.modificarPersona(this.index, persona1);
+    }else {
+      this.personasService.agregarPersona(persona1);
+    }
+    
     this.router.navigate(['personas']);
   }
   
