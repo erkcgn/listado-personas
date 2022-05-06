9dbe05e565e12b6fb74d3385f008f72d8f006baa
Erika <erikac.ec80@gmail.com>
2022-05-06 17:13:52 -0300

    redirecc desde componentes

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 import { LoggingService } from '../../LoggingService.service';
 import { Persona } from '../../persona.model';
 import { PersonasService } from '../../personas.service';
@@ -13,22 +14,25 @@ export class FormularioComponent implements OnInit {
   nombreInput:string = '';
   apellidoInput:string = '';
   
-  constructor(private loggingService:LoggingService,
-              private personasService: PersonasService) {
-                this.personasService.saludar.subscribe(
-                  (indice:number) => alert("El indice es: " + indice)
-                );
-              }
+  constructor(
+    private loggingService:LoggingService,
+    private personasService: PersonasService,
+    private router: Router) {
+      this.personasService.saludar.subscribe(
+        (indice:number) => alert("El indice es: " + indice)
+      );
+    }
   
   ngOnInit() {
 
   }
   
-  agregarPersona(){
+  onGuardarPersona(){
     let persona1 = new Persona(
       this.nombreInput, this.apellidoInput
     );
     this.personasService.agregarPersona(persona1);
+    this.router.navigate(['personas']);
   }
   
 
