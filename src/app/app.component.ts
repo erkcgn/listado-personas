80900283e62d31f5f471956e5294386d44395659
Erika <erikac.ec80@gmail.com>
2022-05-09 21:47:05 -0300

    actualizacion v9 compat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +15,9 @@ export class AppComponent implements OnInit{
   constructor(){}
 
   ngOnInit(): void {
-    
+    firebase.initializeApp({
+      apiKey: "AIzaSyC6mve6u1SM43jWXO8sMfhRY8WAw5oK5G8",
+      authDomain: "listado-personas-69ddc.firebaseapp.com"
+    })
   }    
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,15 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   login(form: NgForm){
     const email = form.value.email;
     const password = form.value.password;
-
+    this.loginService.login(email, password);
   }
 
 }
