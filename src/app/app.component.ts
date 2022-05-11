2b138a558f15d19823442805dacd355f6a670d2f
Erika <erikac.ec80@gmail.com>
2022-05-11 19:52:01 -0300

    revision errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
