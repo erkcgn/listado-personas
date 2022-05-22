b23901e499624cceaff349907d865e77ca2f2290
Erika <erikac.ec80@gmail.com>
2022-05-22 20:52:07 -0300

    SOLUCIONADO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { initializeApp } from 'firebase/app';
 import * as firebase from 'firebase/compat/app';
-import { getDatabase } from "firebase/database"; // agregando esto carga y permite inspeccionar
-//const database = getDatabase();
 
 @Component({
   selector: 'app-root',
@@ -18,20 +15,6 @@ export class AppComponent implements OnInit{
       firebase.default.initializeApp({
         apiKey: "AIzaSyC6mve6u1SM43jWXO8sMfhRY8WAw5oK5G8",
         authDomain: "listado-personas-69ddc.firebaseapp.com"
-      });
-      
-
-    // const firebaseConfig = {
-    //   apiKey: "AIzaSyC6mve6u1SM43jWXO8sMfhRY8WAw5oK5G8",
-    //   authDomain: "listado-personas-69ddc.firebaseapp.com",
-    //   databaseURL: "https://listado-personas-69ddc-default-rtdb.firebaseio.com",
-    //   projectId: "listado-personas-69ddc",
-    //   storageBucket: "listado-personas-69ddc.appspot.com",
-    //   messagingSenderId: "672854108183",
-    //   appId: "1:672854108183:web:df5fe1541b6cf01684d452",
-    //   measurementId: "G-0BSNKZ5WE8"
-    // };
-    // const app = initializeApp(firebaseConfig);
-    
+      });    
   }    
 }
